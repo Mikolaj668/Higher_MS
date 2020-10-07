135847efbd2da13f3b6a636baad6714ea3f81cd6
Mikolaj668 <mikolaj.siwek1@gmail.com>
2020-10-07 21:30:05 +0200

    h4

--- Higher/settings.py
+++ Higher/settings.py
@@ -79,6 +79,10 @@ WSGI_APPLICATION = 'Higher.wsgi.application'
 
 DATABASES = DATABASE_SETTINGS
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
