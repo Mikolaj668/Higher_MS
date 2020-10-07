dae61eaf2e0cd8bcec0ab5ee96aea030b8a9dd17
Mikolaj668 <mikolaj.siwek1@gmail.com>
2020-10-07 21:17:28 +0200

    heroku v4

---
+++ database_private_settings.py
@@ -0,0 +1,8 @@
+DATABASE_SETTINGS = {
+    'default': {
+        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'higher_db',
+        'USER': 'postgres',
+        'PASSWORD': 'M668959482i',
+    }}
